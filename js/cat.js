46564d3c4bde8a52bff48d0b0d87e71cf8bbb90e
fathimafameena <fathima.fameena@yahoo.com>
2018-09-19 11:51:50 -0500

    added two cats and names of each cat using javascript.

--- js/cat.js
+++ js/cat.js
@@ -1,8 +1,21 @@
-var cat=document.getElementById("cat");
-var cattext=document.getElementById("cattext");
+console.log("the value is");
 
+var cat=document.getElementById("cat");
+var cat1=document.getElementById("cat1");
+var cattext=document.getElementById("cattext");
+var catarray=["kiki","muimui","agnes"];
+var cat1name=document.getElementById("cat1name");
+var cat2name=document.getElementById("cat2name");
+
+cat1name.innerText=catarray[0];
+cat2name.innerText=catarray[1];
 let click=0;
 cat.addEventListener('click',function(){
+	click=click+1;
+console.log("no of clicks"+click);
+cattext.value=click;
+});
+cat1.addEventListener('click',function(){
 	click=click+1;
 console.log("no of clicks"+click);
 cattext.value=click;
